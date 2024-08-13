f5b861e282414a71aed323cfd28742d5dc231223
atslotus <441321034@qq.com>
2024-08-13 20:20:40 +0800

    update spatialContrastSensitivityMobile

--- src/pages/ezpsy/spatialContrastSensitivityMobile.vue
+++ src/pages/ezpsy/spatialContrastSensitivityMobile.vue
@@ -61,9 +61,42 @@ const data = reactive({
         angle: 90,
         phase: 0,
         level: 0,
-        gamma: 0.5
-    }
+        gamma: 0.5,
+
+        contrast: 0,
+        SF: 0
+    },
+    canClick: false
 })
+
+const contrastList = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: []
+}
+
+const times = 6;
+const nums = 120;
+const exam_param = [0.5, 1, 2, 4, 8, 16];
+const contrasts = Array.from({ length: times }).map(() => 0.1);
+const exam_list = Array.from({ length: times }).map(() => nums);
+let tips: ezpsy.Texts | null = null
+
+const countTotal = () => {
+    return exam_list.reduce((acc, val) => acc + val, 0)
+}
+
+const mathRandomInt = (a: number, b: number) => {
+    if (a > b) { // Swap a and b to ensure a is smaller.    
+        var c = a;
+        a = b;
+        b = c;
+    }  
+    return Math.floor(Math.random() * (b - a + 1) + a);
+}
 
 onMounted(async () => {
 
@@ -82,17 +115,16 @@ onMounted(async () => {
     const dlg = ezpsy.DlgInit()
     const time = new ezpsy.Time();
 
-    const drawSingrat = async (sf: number, contrast: number) => {
-        const singrat = new ezpsy.sinGrat2({
+    const drawSingrat = async () => {
+        const singrat = new ezpsy.sinGrating1({
             shape: {
-                // x: data.center.x + data.singrat.direction * data.singrat.offset,
-                x: data.center.x,
+                x: data.center.x + data.singrat.direction * data.singrat.offset,
                 y: data.center.y,
                 r: data.singrat.r,
                 pixelsPerDegree: data.singrat.ppd,
-                spatialFrequency: sf,
+                spatialFrequency: data.singrat.SF,
                 angle: data.singrat.angle,
-                contrast: contrast,
+                contrast: data.singrat.contrast,
                 phase: data.singrat.phase,
                 level: data.singrat.level,
                 gamma: data.singrat.gamma
@@ -100,8 +132,31 @@ onMounted(async () => {
             isNoise: false
         })
         await ez.add(singrat)
-        await ezpsy.delay_frame(6)
+        await ezpsy.delay_frame(10)
         singrat.draw()
+        return singrat
+    }
+    const removeSingrat = (singrat: ezpsy.sinGrating1) => {
+        ez.remove(singrat)
+    }
+    const setTips = async () => {
+        !!(tips) && ez.remove(tips)
+        tips = new ezpsy.Texts({
+            shape: {
+                x: data.center.x,
+                y: data.center.y - 150,
+                text: countTotal().toString()
+            },
+            style: {
+                fill: "#333333",
+                fontSize: 40
+            },
+            textLine: {
+                textA: "center",
+                textB: "middle"
+            }
+        })
+        await ez.add(tips)
     }
 
     let bg = new ezpsy.sinGratBG({
@@ -110,8 +165,84 @@ onMounted(async () => {
         }
     })
     await ez.add(bg)
+    let text = new ezpsy.Texts({
+        shape: {
+            x: data.center.x,
+            y: data.center.y,
+            text: "+"
+        },
+        style: {
+            fill: "#333333",
+            fontSize: 80
+        },
+        textLine: {
+            textA: "center",
+            textB: "middle"
+        }
+    })
+    await ez.add(text)
 
-    await drawSingrat(2, 1 / 1000)
+    let circle = new ezpsy.Circle({
+        shape: {
+            x: data.center.x,
+            y: data.center.y,
+            r: 40
+        },
+        style: {
+            fill: "#ff6600"
+        }
+    })
+    await ez.add(circle)
+    await ezpsy.delay_frame(120)
+    ez.remove(circle)
+    await ezpsy.delay_frame(6)
+    await setTips()
+
+    const msg_resp = await dlg.msgDlg({
+        imgUrl: './src/assets/image/ezpsy/spatialContrastSensitivity.png',        
+        title: "",
+        content: '请仔细阅读用户指引',        
+        imgWidth: 240,        
+        imgHeight: 180,        
+        confirm: '开始实验' 
+    })
+
+    if(msg_resp) {
+        document.documentElement.requestFullscreen();
+    }
+    
+    let index = 0
+    // while(countTotal() > 0) {
+        let random = 1
+        if(index == 0) {
+            random = Math.floor(mathRandomInt(1, times))
+            while(exam_list[random - 1] <= 0) {
+                random = Math.floor(mathRandomInt(1, times))
+            }
+            data.singrat.contrast = contrasts[(random - 1)];
+            data.singrat.SF = exam_param[(random - 1)];
+        }
+
+        switch(Math.floor(Math.random() * 2)) {
+            case 0: 
+                data.singrat.direction = -1
+                break
+            case 1:
+                data.singrat.direction = 1
+                break
+            default:
+                console.log("ERR")
+        }
+
+        const singrat = await drawSingrat()
+        data.canClick = true
+        await  ezpsy.delay_frame(30)
+        // removeSingrat(singrat)
+        await ezpsy.delay_frame(60)
+        // if(data.canClick) {
+
+        // }
+    // }
 
 })
 </script>
